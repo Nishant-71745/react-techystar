c03756a469fe6d90e9a9415dc2dc9328ab71df96
Nishant-71745 <nishant71745@gmail.com>
2024-06-23 11:03:04 +0530

    add files

---
+++ 02Project/src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://instagram.com/n.i.s.h.a.n.t_71745" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Nishant-71745" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
